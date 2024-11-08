490d86887bd7d407c7de125d9a1f86694d74e189
enarl <enar.leini00@gmail.com>
2024-11-08 16:41:26 +0200

    set up initial backend and frontend

---
+++ backend/src/test/java/com/example/auto24/Auto24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auto24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Auto24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
